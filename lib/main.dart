80725a2abf0189620770ee442bcad8b8764b3fd7
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2023-02-14 22:28:28 -0300

    feat(main): await for database initialization at main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:notebook/src/app_widget.dart';
+import 'package:notebook/src/database/database_client.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DatabaseClient.instance.init();
   runApp(const AppWidget());
 }
