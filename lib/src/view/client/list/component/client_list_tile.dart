64f9741dad21debf86b9090ebe35a8840254de57
Alcides Ribeiro <alcidesribeirosampaioneto@gmail.com>
2023-02-20 20:08:47 -0300

    feat(client list tile): add confirmation to remove client

--- lib/src/view/client/list/component/client_list_tile.dart
+++ lib/src/view/client/list/component/client_list_tile.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
 
-import '../../../../controller/client_controller.dart';
 import '../../../../model/client_model.dart';
+import 'client_popup_menu_button.dart';
 
 class ClientListTile extends StatelessWidget {
   const ClientListTile({super.key, required this.client});
   final ClientModel client;
 
-  String formatCurrency(num number) =>
-      NumberFormat.currency(locale: 'pt_BR', name: '', decimalDigits: 2)
+  String formatCurrency(num number) {
+    return NumberFormat.currency(locale: 'pt_BR', name: '', decimalDigits: 2)
           .format(number * 0.01);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final clientController =
-        Provider.of<ClientController>(context, listen: false);
     return ListTile(
       leading: CircleAvatar(
         backgroundColor: Theme.of(context).colorScheme.primary,
@@ -25,22 +23,10 @@ class ClientListTile extends StatelessWidget {
       ),
       title: Text(client.name),
       subtitle: Text(formatCurrency(client.balance)),
-      onTap: () =>
-          Navigator.of(context).pushNamed('/operation/list', arguments: client),
-      trailing: PopupMenuButton(
-        icon: const Icon(Icons.more_vert),
-        itemBuilder: (BuildContext context) {
-          return [
-            const PopupMenuItem(
-              child: Text('Editar'),
-            ),
-            PopupMenuItem(
-              onTap: () => clientController.remove(client),
-              child: const Text('Remover'),
-            ),
-          ];
-        },
-      ),
+      onTap: () {
+        Navigator.of(context).pushNamed('/operation/list', arguments: client);
+      },
+      trailing: ClientPopupMenuButton(client: client),
     );
   }
 }
---
+++ lib/src/view/client/list/component/client_popup_menu_button.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'dart:developer' as dev;
+
+import '../../../../controller/client_controller.dart';
+import '../../../../model/client_model.dart';
+import '../../../../util/popup_menu_option.dart';
+
+class ClientPopupMenuButton extends StatelessWidget {
+  const ClientPopupMenuButton({
+    super.key,
+    required this.client,
+  });
+
+  final ClientModel client;
+
+  @override
+  Widget build(BuildContext context) {
+    final clientController =
+        Provider.of<ClientController>(context, listen: false);
+    return PopupMenuButton<PopupMenuOption>(
+      icon: const Icon(Icons.more_vert),
+      onSelected: (value) {
+        switch (value) {
+          case PopupMenuOption.edit:
+            dev.log('não implementado');
+            break;
+
+          case PopupMenuOption.remove:
+            deleteConfirmationDialog(context).then((value) {
+              if (value ?? false) {
+                clientController.remove(client);
+              }
+            });
+            break;
+        }
+      },
+      itemBuilder: (BuildContext context) {
+        return [
+          PopupMenuItem(
+            value: PopupMenuOption.edit,
+            child: Text(PopupMenuOption.edit.title),
+          ),
+          PopupMenuItem(
+            value: PopupMenuOption.remove,
+            child: Text(PopupMenuOption.remove.title),
+          ),
+        ];
+      },
+    );
+  }
+
+  Future<bool?> deleteConfirmationDialog(BuildContext context) async {
+    return await showDialog<bool?>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Aviso'),
+          content: const Text('Deseja remover esse cliente?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: Text(
+                'Não',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.error,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text('Sim'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
